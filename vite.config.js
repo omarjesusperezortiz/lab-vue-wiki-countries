9c3265fa15cf04c3f4cbe98c500b6e6d3fe7fe6c
omarjesusperezortiz <omarjesus.perezortiz@gmail.com>
2022-11-22 01:14:25 +0100

    cambios en yarn build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { outDir: 'docs'},
-  // base:'/lab-vue-wiki-countries/',
+  base:'/lab-vue-wiki-countries/',
   plugins: [vue()]
 })
